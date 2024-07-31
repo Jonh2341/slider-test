6919d5601e9ac9c9ade0819dcb5a3a130511ef02
Adam <rokakaak@Gmail.com>
2024-07-31 17:43:02 +0300

    init

--- src/Pallet.js
+++ src/Pallet.js
@@ -1,17 +1,17 @@
 import { Slider } from "@mui/material";
 
+function colorChange({Slider, container}) {
+    if (Slider.value >= 30) {
+        container.style.backgroundColor = "red";
+    }
+}
+
 function Pallete() {
     return (
         <div className="container">
             <Slider defaultValue={0} aria-label="Default" onChange={colorChange} valueLabelDisplay="auto" />
         </div>
     );
-}
-
-function colorChange({Slider, container}) {
-    if (Slider.value >= 30) {
-        container.style.backgroundColor = "red";
-    }
 }
 
 export default Pallete;
